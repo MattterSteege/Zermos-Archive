9edf839a8b8dc1ccea547d7d78ee4a8c7c97d0ce
Kronk <78482881+MJTSgamer@users.noreply.github.com>
2022-10-30 20:56:51 +0100

    Delete Assets directory

--- Assets/Prefabs/Homework/homeworkDivider.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
-
-public class homeworkDivider : MonoBehaviour
-{
-    [SerializeField] public TMP_Text Datum;
-}
