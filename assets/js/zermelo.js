4eafed7548d90bb37375c5b5b26ca7cc6f60f310
MJTSgamer <matttersteege@gmail.com>
2023-04-21 09:22:19 +0200

    better file orginization, some rooster updates, NICE

--- assets/js/zermelo.js
+++ assets/js/zermelo.js
@@ -1,16 +1,22 @@
 function getDagrooster(){
     let ajaxRequest = new XMLHttpRequest();
-    ajaxRequest.onreadystatechange = function(){
-        console.log("Ready state changed!");
-        //more on this in a second
-    }
-    ajaxRequest.open("GET", "https://ccg.zportal.nl/api/v3/liveschedule?access_token=gvcnr9ck37l96uk70u4dcdbuq8&student=58373&week=202316", true);
+    const access_token = localStorage.getItem("zermelo-access_token");
+    const student = localStorage.getItem("zermelo-student_id");
+
+
+    let currentDate = new Date();
+    let startDate = new Date(currentDate.getFullYear(), 0, 1);
+    let days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
+    const weekNumber = Math.ceil(days / 7);
+
+    ajaxRequest.open("GET", "https://ccg.zportal.nl/api/v3/liveschedule?access_token=" + access_token + "&student=" + student + "&week=2023" + weekNumber, true);
     ajaxRequest.send();
 
     ajaxRequest.onreadystatechange = function(){
         if(ajaxRequest.readyState === 4){
             //navigator.clipboard.writeText(ajaxRequest.responseText);
             const data = JSON.parse(ajaxRequest.responseText);
+            console.log(data);
 
             const model = {
                 response: {
@@ -85,8 +91,18 @@ function getDagrooster(){
                 for (let i = 0; i < appointmentDay.length; i++) {
                     if (appointmentDay[i].startTimeSlotName === lessonNumber.toString()) {
                         const gridItem = document.createElement("div");
+
                         gridItem.classList.add("grid-item");
-                        gridItem.textContent = appointmentDay[i].subjects.length > 0 ? appointmentDay[i].subjects[0] : "";
+                        gridItem.textContent = (appointmentDay[i].subjects.length > 0 ? appointmentDay[i].subjects[0] : "") + (appointmentDay[i].locations[0] ? " - " + appointmentDay[i].locations[0] : "") + (appointmentDay[i].teachers[0] ? " - " + appointmentDay[i].teachers[0] : "");
+
+                        if(appointmentDay[i].cancelled === true && gridItem.textContent !== ""){
+                            gridItem.classList.add("cancelled");
+                        }
+
+                        //href to ./lesson?id=[id]
+                        gridItem.addEventListener("click", function(){
+                            window.location.href = "./lesson/?id=" + appointmentDay[i].id;
+                        });
 
                         gridColumn.appendChild(gridItem);
                     } else {
@@ -109,4 +125,28 @@ function getDagrooster(){
             }
         }
     }
+}
+
+getWeek = function() {
+    var date = new Date(this.getTime());
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    // January 4 is always in week 1.
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+        - 3 + (week1.getDay() + 6) % 7) / 7);
+}
+
+// Returns the four-digit year corresponding to the ISO week of the date.
+getWeekYear = function() {
+    var date = new Date(this.getTime());
+    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    return date.getFullYear();
+}
+
+function getLessonFromParameter(){
+    //add div with content: window.location.search.replace("?id=", "")
+    document.getElementById("lesson").textContent = window.location.search.replace("?id=", "");
 }
