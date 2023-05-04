bd84406ac6f4a197a264d12462f24dbb28eac9df
MJTSgamer <matttersteege@gmail.com>
2023-05-04 19:52:24 +0200

    Implemented Homework

--- assets/js/somtoday.js
+++ assets/js/somtoday.js
@@ -1,3 +1,5 @@
+console.log("SOMtoday.js loaded successfully");
+
 function authenticateUser(username, password) {
     const xhr = new XMLHttpRequest();
     const url = `https://localhost:44333/SOMtoday/authenticate?username=${username}&password=${password}`;
@@ -63,6 +65,8 @@ function CheckIfSOmTodayTokenIsExpired() {
                 localStorage.setItem("somtoday-access_token", model.access_token);
                 localStorage.setItem("somtoday-refresh_token", model.refresh_token);
             }
+
+            getStudent();
         }
     }
 }
@@ -134,8 +138,6 @@ function getGrades() {
                 omschrijving: item.omschrijving,
             }));
 
-            console.log(model);
-
             // count all grades with the same vak.naam and calculate the average, then display it in the table like this:
             // <div className="grade-child">
             //     <div className="left">
@@ -157,6 +159,7 @@ function getGrades() {
             let currentVak = "";
             let currentVakWeight = 0;
             let currentVakGradeTotal = 0;
+            let currentVakId = 0;
 
             model.forEach(grade => {
                 if (grade.type === "DeeltoetsKolom"){
@@ -167,18 +170,21 @@ function getGrades() {
                     currentVak = grade.vak.naam;
                     currentVakWeight += Number(grade.weging) === 0 ? Number(grade.examenWeging) : Number(grade.weging);
                     currentVakGradeTotal += (Number(grade.geldendResultaat.replace(",", "")) / 10) * (Number(grade.weging) === 0 ? Number(grade.examenWeging) : Number(grade.weging));
+                    currentVakId = Number(grade.vak.links[0].id);
                 } else {
                     if (currentVak !== "") {
                         ListOfGradesWithSameVakNaam.push({
                             vak: currentVak,
                             weight: currentVakWeight,
-                            grade: Math.round((currentVakGradeTotal / currentVakWeight) * 10) / 10
+                            grade: Math.round((currentVakGradeTotal / currentVakWeight) * 10) / 10,
+                            id: Number(currentVakId)
                         });
                     }
 
                     currentVak = grade.vak.naam;
                     currentVakWeight = Number(grade.weging) === 0 ? Number(grade.examenWeging) : Number(grade.weging);
                     currentVakGradeTotal = (Number(grade.geldendResultaat.replace(",", "")) / 10) * (Number(grade.weging) === 0 ? Number(grade.examenWeging) : Number(grade.weging));
+                    currentVakId = Number(grade.vak.links[0].id);
                 }
             });
 
@@ -186,10 +192,12 @@ function getGrades() {
             ListOfGradesWithSameVakNaam.push({
                 vak: currentVak,
                 weight: currentVakWeight,
-                grade: Math.round((currentVakGradeTotal / currentVakWeight) * 10) / 10
+                grade: Math.round((currentVakGradeTotal / currentVakWeight) * 10) / 10,
+                id: Number(currentVakId)
             });
 
 
+            //console.log(ListOfGradesWithSameVakNaam);
 
 
 
@@ -209,7 +217,7 @@ function getGrades() {
             // parent id = grade-container
 
             ListOfGradesWithSameVakNaam.forEach(grade => {
-                console.log(grade);
+                //console.log(grade);
                 const gradeContainer = document.getElementById("grade-container");
 
                 const gradeChild = document.createElement("div");
@@ -231,6 +239,118 @@ function getGrades() {
                 right.className = "right";
                 right.innerText = grade.grade;
                 if (grade.grade >= 5.5 || isNaN(grade.grade)) {
+                    right.classList.add("passed");
+                }
+                else {
+                    right.classList.add("failed");
+                }
+
+                gradeChild.appendChild(left);
+                gradeChild.appendChild(weight);
+                gradeChild.appendChild(right);
+
+                gradeContainer.appendChild(gradeChild);
+                /*on click gradeChild*/
+                gradeChild.addEventListener("click", () => {
+                    window.location.href = "/Zermos/somtoday/cijfers/vak/?" + grade.id;
+                });
+            });
+        }
+    }
+
+    xhr.send();
+}
+
+
+function getGradesById(vakId) {
+    CheckIfSOmTodayTokenIsExpired();
+    const token = localStorage.getItem("somtoday-access_token");
+    const studentId = localStorage.getItem("somtoday-student_id");
+    const begintNaOfOp = "2022-01-01";
+
+    if (token === null) {
+        //console.log("No access token or student id found, redirecting to login page...");
+        window.location.href = "/Zermos/somtoday/inloggen/";
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://localhost:44333/SOMtoday/gradesById?token=${token}&studentId=${studentId}&begintNaOfOp=${begintNaOfOp}&vakId=${vakId}`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            const response = JSON.parse(this.responseText);
+            const model = response.items.map(item => ({
+                type: item.type,
+                links: item.links.map(link => ({
+                    id: link.id,
+                    rel: link.rel,
+                    type: link.type,
+                    href: link.href
+                })),
+                permissions: item.permissions.map(permission => ({
+                    full: permission.full,
+                    type: permission.type,
+                    operations: permission.operations,
+                    instances: permission.instances
+                })),
+                herkansingstype: item.herkansingstype,
+                resultaat: item.resultaat,
+                geldendResultaat: item.geldendResultaat,
+                datumInvoer: item.datumInvoer,
+                teltNietmee: item.teltNietmee,
+                toetsNietGemaakt: item.toetsNietGemaakt,
+                leerjaar: item.leerjaar,
+                periode: item.periode,
+                weging: item.weging,
+                examenWeging: item.examenWeging,
+                isExamendossierResultaat: item.isExamendossierResultaat,
+                isVoortgangsdossierResultaat: item.isVoortgangsdossierResultaat,
+                vak: {
+                    links: item.vak.links.map(link => ({
+                        id: link.id,
+                        rel: link.rel,
+                        type: link.type,
+                        href: link.href
+                    })),
+                    permissions: item.vak.permissions.map(permission => ({
+                        full: permission.full,
+                        type: permission.type,
+                        operations: permission.operations,
+                        instances: permission.instances
+                    })),
+                    additionalObjects: item.vak.additionalObjects,
+                    afkorting: item.vak.afkorting,
+                    naam: item.vak.naam
+                },
+                vrijstelling: item.vrijstelling,
+                omschrijving: item.omschrijving,
+            }));
+
+
+            model.forEach(grade => {
+                //console.log(grade);
+                const gradeContainer = document.getElementById("grade-container");
+
+                const gradeChild = document.createElement("div");
+                gradeChild.className = "grade-child";
+
+                const left = document.createElement("div");
+                left.className = "left";
+
+                const subject = document.createElement("p");
+                subject.className = "description";
+                subject.innerText = grade.omschrijving;
+                left.appendChild(subject);
+
+                const weight = document.createElement("div");
+                weight.className = "weight";
+                weight.innerText = `${Number(grade.weging) === 0 ? Number(grade.examenWeging) : Number(grade.weging)}x`;
+
+                const right = document.createElement("div");
+                right.className = "right";
+                right.innerText = grade.geldendResultaat;
+                if (grade.geldendResultaat >= 5.5 || isNaN(grade.geldendResultaat)) {
                     right.classList.add("passed");
                 }
                 else {
@@ -278,4 +398,137 @@ function getStudent() {
     }
 
     xhr.send();
+}
+
+function getHomework(){
+    CheckIfSOmTodayTokenIsExpired();
+    //GET: https://localhost:44333/SOMtoday/huiswerk?begintNaOfOp=2022-01-10&token=
+    CheckIfSOmTodayTokenIsExpired();
+    //const begintNaOfOp = current date yyyy-mm-dd minus 14 days
+    const begintNaOfOp = new Date();
+    begintNaOfOp.setDate(begintNaOfOp.getDate() - 14);
+    const begintNaOfOpString = `${begintNaOfOp.getFullYear()}-${begintNaOfOp.getMonth() + 1}-${begintNaOfOp.getDate()}`;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://localhost:44333/SOMtoday/huiswerk?begintNaOfOp=${begintNaOfOpString}&token=${localStorage.getItem("somtoday-access_token")}`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4){
+            //console.log(this.responseText);
+            const response = JSON.parse(this.responseText);
+            //console.log(response);
+            const model = response.items.map(item => ({
+                aangemaaktOpDatumTijd: item.aangemaaktOpDatumTijd,
+                additionalObjects: {
+                    huiswerkgemaakt: item.additionalObjects.huiswerkgemaakt,
+                    //leerlingen: item.additionalObjects.leerlingen,
+                    swigemaaktVinkjes: item.additionalObjects.swigemaaktVinkjes
+                },
+                datumTijd: item.datumTijd,
+                lesgroep: {
+                    naam: item.lesgroep.naam,
+                    schooljaar: {
+                        naam: item.lesgroep.schooljaar.naam,
+                        totDatum: item.lesgroep.schooljaar.totDatum,
+                        vanafDatum: item.lesgroep.schooljaar.vanafDatum
+                    },
+                    vak: {
+                        afkorting: item.lesgroep.vak.afkorting,
+                        naam: item.lesgroep.vak.naam
+                    },
+                    vestiging: {
+                        naam: item.lesgroep.vestiging.naam
+                    },
+                },
+                sortering: item.sortering,
+                studiewijzerItem: {
+                    bijlagen: item.studiewijzerItem?.bijlagen ?? [],
+                    huiswerkType: item.studiewijzerItem?.huiswerkType ?? "",
+                    omschrijving: item.studiewijzerItem?.omschrijving ?? "",
+                    onderwerp: item.studiewijzerItem?.onderwerp ?? "",
+                    tonen: item.studiewijzerItem?.tonen ?? true
+                }
+            }));
+
+            //return;
+
+
+            model.sort((a, b) => {
+                return new Date(a.datumTijd) - new Date(b.datumTijd);
+            });
+
+
+            let currentDate = "";
+            let currentHomeworkContainer = null;
+            model.forEach(item => {
+                const date = item.datumTijd.split("T")[0];
+                if (date !== currentDate) {
+                    const homeworkContainer = document.getElementById("homework-container");
+                    const homeworkDateDivider = document.createElement("div");
+                    homeworkDateDivider.className = "homework-date-divider";
+
+                    const dateObject = new Date(date);
+                    const day = dateObject.toLocaleString("nl-NL", {weekday: "long"});
+                    const dayNumber = dateObject.toLocaleString("nl-NL", {day: "numeric"});
+                    const month = dateObject.toLocaleString("nl-NL", {month: "long"});
+                    homeworkDateDivider.innerText = `${day} ${dayNumber} ${month}`;
+
+                    const homeworkContainerForCurrentHomework = document.createElement("div");
+                    homeworkContainerForCurrentHomework.className = "row";
+
+                    homeworkContainer.appendChild(homeworkDateDivider);
+                    homeworkContainer.appendChild(homeworkContainerForCurrentHomework);
+                    currentDate = date;
+                    currentHomeworkContainer = homeworkContainerForCurrentHomework;
+                }
+
+                /*
+                      <div class="homework-child">
+                        <div class="left">
+                          <h1 class="subject">[vak.naam]</h1>
+                          <p class="description">omschrijving</p>
+                        </div>
+                        <div class="right passed">
+
+                        </div>
+                      </div>
+                */
+
+                const homeworkChild = document.createElement("div");
+                homeworkChild.className = "homework-child";
+
+                const left = document.createElement("div");
+                left.className = "left";
+
+                const subject = document.createElement("h1");
+                subject.className = "subject";
+                subject.innerText = item.lesgroep.vak.naam;
+
+                const description = document.createElement("p");
+                description.className = "description";
+                description.innerText = removeHTMLTags(item.studiewijzerItem.omschrijving === "" ? item.studiewijzerItem.onderwerp : item.studiewijzerItem.omschrijving);
+
+                left.appendChild(subject);
+                left.appendChild(description);
+
+
+
+                const right = document.createElement("div");
+                right.className = "right";
+
+                homeworkChild.appendChild(left);
+                homeworkChild.appendChild(right);
+
+                currentHomeworkContainer.appendChild(homeworkChild);
+            });
+
+        }
+    }
+
+    xhr.send();
+}
+
+function removeHTMLTags(text) {
+    return text.replace(/<[^>]*>/g, '');
 }
