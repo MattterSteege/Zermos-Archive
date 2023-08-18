d62f62899059e26bc84e7992ebbf41c77cb2411d
MJTSgamer <matttersteege@gmail.com>
2023-08-18 12:26:42 +0200

    DONE.txt

--- Zermos-Web/wwwroot/js/global.js
+++ Zermos-Web/wwwroot/js/global.js
@@ -1,20 +1,20 @@
-﻿    console.log('[ GLOBAL.JS ] loaded');
-document.addEventListener('DOMContentLoaded', function () {
+﻿console.log('[ GLOBAL.JS ] loaded');
+document.addEventListener('DOMContentLoaded', function() {
     const elements = document.querySelectorAll('.svgText');
     // console.log(elements);
 
-    elements.forEach(function (element) {
-        element.addEventListener('mouseover', function () {
+    elements.forEach(function(element) {
+        element.addEventListener('mouseover', function() {
             element.classList.add('animating');
         });
 
-        element.addEventListener('transitionend', function () {
+        element.addEventListener('transitionend', function() {
             element.classList.remove('animating');
         });
     });
 
 
-//remove the version after 5 seconds, fade
+    //remove the version after 5 seconds, fade
     setTimeout(() => {
         //fade .version out in 1s
         $(".version").fadeOut(1000, () => {
@@ -25,11 +25,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const delay = ms => new Promise(res => setTimeout(res, ms));
 
-//for each notification, add the .animate class, but only if the previous notification has been animated with an delay of 500ms, the first one also gets a delay of 500ms
+    //for each notification, add the .animate class, but only if the previous notification has been animated with an delay of 500ms, the first one also gets a delay of 500ms
 
     async function animateNotifications() {
         const notifications = $(".notification");
-        
+
         let charsCount = 0;
 
         notifications.removeClass("animate");
@@ -41,11 +41,11 @@ document.addEventListener('DOMContentLoaded', function () {
             $(notification).addClass("animate");
 
             //when clicked: $(notification).removeClass("animate");
-            notification.addEventListener('click', function () {
+            notification.addEventListener('click', function() {
                 $(notification).removeClass("animate");
             });
         }
-        
+
         //a person reads around 265 characters per minute, as calculated here: https://ux.stackexchange.com/questions/22520/how-long-does-it-take-to-read-x-number-of-characters
         //so we wait 2 seconds and add 60 seconds for each 265 words, with a character average if 4.7 per word, which means +/- 1245,5 characters per minute
         //so: 2 seconds + 60 seconds for each 1245,5 characters
@@ -82,8 +82,8 @@ function addNotification(title, body, type) {
     new Promise(res => setTimeout(res, 500)).then(() => {
         $(notification).addClass("animate");
 
-        notification.addEventListener('click', function () {
-                $(notification).removeClass("animate");
+        notification.addEventListener('click', function() {
+            $(notification).removeClass("animate");
         }, false);
 
         new Promise(res => setTimeout(res, 5000)).then(() => {
@@ -92,4 +92,15 @@ function addNotification(title, body, type) {
     });
 
     return notification;
+}
+
+async function Update() {
+    /*                                    */
+    /*                                    */
+    /*    Nee, je moet Update(); typen    */  
+    /*                                    */
+    /*                                    */
+
+    console.log('[ GLOBAL.JS ] Update uitvoeren');
+    window.location.href = "/";
 }
