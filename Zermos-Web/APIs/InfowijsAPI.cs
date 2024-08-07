c6792dc18f39c6410caeb298257726627778d043
MJTSgamer <matttersteege@gmail.com>
2024-08-07 13:03:58 +0200

    added a better approximation of the infowijs message since param.

--- Zermos-Web/APIs/InfowijsAPI.cs
+++ Zermos-Web/APIs/InfowijsAPI.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -32,11 +33,48 @@ namespace Zermos_Web.APIs
         
         public async Task<InfowijsMessagesModel> GetSchoolNieuwsAsync(user user)
         {
+#if DEBUG
+            var watch = Stopwatch.StartNew();
+#endif
+            //y=0.0508x−81148563 (approxSince = 0.0508 * DateTime.Now.ToUnixTimeSeconds() - 81148563)
+            var approxSince = (int) (0.0489 * DateTime.Now.ToUnixTime() - 78164184) - 750000;
+            
             var json = TokenUtils.DecodeJwt(user.infowijs_access_token);
             var client = json.payload.customerProduct.name;
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await GetSessionTokenAsync(user));
-            var response = await _httpClient.GetAsync($"https://{client}.hoyapp.nl/hoy/v3/messages?include_archived=0&since=5500000");
+            var response = await _httpClient.GetAsync($"https://{client}.hoyapp.nl/hoy/v3/messages?include_archived=1&since={approxSince}");
+            
+#if DEBUG
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds + "ms");
+#endif
+            
             return JsonConvert.DeserializeObject<InfowijsMessagesModel>(await response.Content.ReadAsStringAsync(), Converter.Settings);
+            
+            // var json = TokenUtils.DecodeJwt(user.infowijs_access_token);
+            // var client = json.payload.customerProduct.name;
+            // _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await GetSessionTokenAsync(user));
+            // var response = await _httpClient.GetAsync($"https://{client}.hoyapp.nl/hoy/v3/messages?include_archived=1&since=0");
+            //
+            // InfowijsMessagesModel model = JsonConvert.DeserializeObject<InfowijsMessagesModel>(await response.Content.ReadAsStringAsync(), Converter.Settings);
+            //
+            // long since = model.Data.Since;
+            //
+            // bool shouldContinue = true;
+            //
+            // while (shouldContinue)
+            // {
+            //     //https://{client}.hoyapp.nl/hoy/v3/messages?include_archived=1&{since}
+            //     response = await _httpClient.GetAsync($"https://{client}.hoyapp.nl/hoy/v3/messages?include_archived=1&since={since}");
+            //     InfowijsMessagesModel newModel = JsonConvert.DeserializeObject<InfowijsMessagesModel>(await response.Content.ReadAsStringAsync(), Converter.Settings);
+            //     model.Data.Messages.AddRange(newModel.Data.Messages);
+            //     since = newModel.Data.Since;
+            //     shouldContinue = newModel.Data.HasMore;
+            // }
+            
+            //return model;
+            
+            //return JsonConvert.DeserializeObject<InfowijsMessagesModel>(await response.Content.ReadAsStringAsync(), Converter.Settings);
             
             /*
                 type catalog:
